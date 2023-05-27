5415451ad08f1a7b4c8e129101356beec69f52eb
AliceAdeseun <aliceadenikeadeseun@gmail.com>
2023-05-27 16:12:41 +0100

    updated the number of instances

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_vpc" "my_vpc" {
   instance_tenancy     = "default"
   tags = {
     name = "ApacheVPC"
+  
   }
 }
 
@@ -84,6 +85,7 @@ resource "aws_internet_gateway" "cba_igw" {
 resource "aws_route_table_association" "cba_subnet_rt_public" {
   subnet_id      = aws_subnet.cba_public.id
   route_table_id = aws_route_table.cba_public_rt.id
+  count = 2
 }
 data "aws_ssm_parameter" "instance_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
@@ -95,6 +97,7 @@ resource "aws_instance" "cba_tf_instance" {
   security_groups = [aws_security_group.cba_tf_sg.id]
   key_name        = var.key_name
   user_data       = fileexists("install_apache.sh") ? file("install_apache.sh") : null
+  
 
 
   tags = {
