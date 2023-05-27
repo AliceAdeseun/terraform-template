c0a5f5c1c547fc3c51933a3f1572725125f49344
AliceAdeseun <aliceadenikeadeseun@gmail.com>
2023-05-27 15:44:12 +0100

    my first commit

---
+++ install_apache.sh
@@ -0,0 +1,9 @@
+
+
+#!/bin/bash
+sudo su
+yum update -y
+yum install httpd -y
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
